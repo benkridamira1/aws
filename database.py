d0ecf4f02c143920834a5c4ae2768911af5f45fa
benkridamira1 <77399009+benkridamira1@users.noreply.github.com>
2022-12-09 22:12:55 +0100

    change in db

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://user_name:password@endpoint/postgres"
+SQLALCHEMY_DATABASE_URL = "postgresql://amira:amira1234@database-2.cwccje4mphn1.us-east-1.rds.amazonaws.com/postgres"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
